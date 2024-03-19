729a762411373a0584324c4cc759752e9a07faa9
0320kangk <0320kangk@naver.com>
2024-03-19 18:32:03 +0900

    Revert "key Appsk 세팅"
    This reverts commit 32dae4d00c259bca5ae50ac0c6c8a0fd9cb0bb04.

--- src/main/java/project/keyappsk/domain/product/entity/enumerate/ProductStatus.java
+++
@@ -1,6 +0,0 @@
-package project.keyappsk.domain.product.entity.enumerate;
-
-public enum ProductStatus {
-    DEADLIEN,
-    SALE
-}
--- src/main/java/project/keyappsk/domain/store/entity/enumerate/StoreStatus.java
+++
@@ -1,5 +0,0 @@
-package project.keyappsk.domain.store.entity.enumerate;
-
-public enum StoreStatus {
-    open, close
-}
