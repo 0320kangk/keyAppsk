4d147b3e57fdad47b25f9eb864b9782e3e3767e4
0320kangk <0320kangk@naver.com>
2024-03-19 18:38:14 +0900

    Revert "Revert "key Appsk 세팅""
    This reverts commit 729a762411373a0584324c4cc759752e9a07faa9.

---
+++ src/test/java/project/keyappsk/KeyAppskApplicationTests.java
@@ -0,0 +1,13 @@
+package project.keyappsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyAppskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
