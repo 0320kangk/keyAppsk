6026410905f7db69617047b95ffb9353d7769915
0320kangk <0320kangk@naver.com>
2024-03-28 22:39:47 +0900

    CustomUserDetails 클래스 생성, userDetails,와 Oauth2User 통합, 이제 로그인 했을시 같은 객체로 관리

--- src/main/java/project/keyappsk/domain/member/controller/MemberController.java
+++ src/main/java/project/keyappsk/domain/member/controller/MemberController.java
@@ -2,15 +2,19 @@ package project.keyappsk.domain.member.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import project.keyappsk.domain.member.dto.CustomUserDetails;
 import project.keyappsk.domain.member.dto.LoginMemberDto;
 import project.keyappsk.domain.member.service.MemberService;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
     private final MemberService memberService;
     @GetMapping("/member/add")
@@ -23,7 +27,8 @@ public class MemberController {
         return "content/sign";
     }
     @GetMapping("/member/login")
-    String getLoginPage(){
+    String getLoginPage(@AuthenticationPrincipal CustomUserDetails customUserDetails ){
+        if( customUserDetails != null) log.info("userDetails: {}", customUserDetails);
         return "content/login";
     }
 
