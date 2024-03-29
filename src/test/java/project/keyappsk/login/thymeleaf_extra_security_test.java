6210e5a96b7d96036aaa6bbb7293ea2fb3625edc
0320kangk <0320kangk@naver.com>
2024-03-29 16:29:28 +0900

    로그아웃 로그인 버튼 생성

--- src/test/java/project/keyappsk/login/thymeleaf_extra_security_test.java
+++ src/test/java/project/keyappsk/login/thymeleaf_extra_security_test.java
@@ -1,14 +1,18 @@
 package project.keyappsk.login;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.DispatcherServlet;
 import project.keyappsk.domain.member.dto.SessionMember;
 import project.keyappsk.global.consts.SessionConst;
 
@@ -19,18 +23,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 public class thymeleaf_extra_security_test {
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private HttpSession httpSession;
-    @BeforeEach
-    void session(){
-        httpSession.setAttribute(SessionConst.SessionMember, new SessionMember("junho","0320kangk@"));
-    }
+
 
     @Test
     @WithMockUser(username = "junho",  roles = "GUEST")
     void 포스트작성() throws Exception {
-        httpSession.setAttribute(SessionConst.SessionMember, new SessionMember("junho","0320kangk@"));
+        MockHttpSession mockHttpSession = new MockHttpSession();
+        mockHttpSession.setAttribute("junho", "0320kangk");
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
-                .andDo(print());
+//                        .session(mockHttpSession))
+                         .andDo(print());
     }
 }
